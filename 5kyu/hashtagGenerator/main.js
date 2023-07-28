69a47f50b6c3e49f6d8b3b143911dbfc5309bdc5
filomenoPonce580 <filomenoponce580@gmail.com>
2023-07-27 21:12:57 -0700

    touched up function, easier to follow visually. same time complexity

--- 5kyu/hashtagGenerator/main.js
+++ 5kyu/hashtagGenerator/main.js
@@ -1,13 +1,9 @@
 function generateHashtag (str) {
-    if (str === '' || str.split(" ").filter(x=>x!=='').length===0) return false
-    let array = str.split(' ').map((word)=>{
-      if (!word.trim()) {
-        return '';
-      }
-      return word[0].toUpperCase() + word.slice(1)
-    })
+    if(!str || str.split(" ").filter(x=>x!=='').length===0) return false;
     
-    array.unshift('#')
-    
-    return array.join('').length <= 140 ? array.join('') : false
+    let result = '#' + str.split(' ').map( word => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }).join('');
+ 
+    return result.length > 140 ? false : result;
 }
