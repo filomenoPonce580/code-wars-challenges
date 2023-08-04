e592788c4a061dc30470fe591fa7baaa517253ba
filomenoPonce580 <filomenoponce580@gmail.com>
2023-08-04 15:38:27 -0700

    complete

--- 6kyu/threeAddedCharacters/main.js
+++ 6kyu/threeAddedCharacters/main.js
@@ -1,9 +1,15 @@
 function addedChar(s1, s2){
-    let one = Array.from(new Set(s1.split('').sort()))
-    let two = Array.from(new Set(s2.split('').sort().reverse()))
-    
-    let cutOff = one.length
-    
-    console.log(one, two)
-    return two.slice(cutOff).join('')
+    let one = [...s1].sort()
+    let two = [...s2].sort()
+    return two.find((char, i) => char !== one[i])
 }
+
+// function addedChar(s1, s2){
+//     let one = Array.from(new Set(s1.split('').sort()))
+//     let two = Array.from(new Set(s2.split('').sort().reverse()))
+    
+//     let cutOff = one.length
+    
+//     console.log(one, two)
+//     return two.slice(cutOff).join('')
+// }
