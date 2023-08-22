f50875101a08cc4a3b5418ca70920834ecc66d9d
filomenoPonce580 <filomenoponce580@gmail.com>
2023-08-22 14:37:23 -0700

    office find chairs complete

--- 6kyu/theOfficeVFindAChair/main.js
+++ 6kyu/theOfficeVFindAChair/main.js
@@ -1,19 +1,39 @@
-function meeting(x, need){
-    if(need===0) return 'Game On'
+// function meeting(x, need){
+//     if(need===0) return 'Game On'
   
-    let result = []
-    for(let i = 0; i < x.length; i++){
-      let chairs = x[i][1]
-      let occupied = x[i][0].length
-      let available = chairs - occupied
-      if(available <= 0){
-        result.push(0)
-      } else {
-        result.push(available)
-      }
+//     let result = []
+//     for(let i = 0; i < x.length; i++){
+//       let chairs = x[i][1]
+//       let occupied = x[i][0].length
+//       let available = chairs - occupied
+//       if(available <= 0){
+//         result.push(0)
+//       } else {
+//         result.push(available)
+//       }
       
-      if(result.reduce((a,c)=>a+c,0) >= need) return result
-    }
+//       if(result.reduce((a,c)=>a+c,0) >= need) return result
+//     }
     
-    return "Not enough!"
+//     return "Not enough!"
+// }
+
+function meeting(x, need){
+  
+  let borrow = [];
+  
+  if (!need) 
+    return 'Game On';
+    
+  for (let [ used, chairs ] of x) {
+    chairs = Math.min(need, Math.max(0, chairs - used.length));
+    borrow.push(chairs);
+    need -= chairs;
+    
+    if (!need)
+      return borrow;
+  }
+  
+  return 'Not enough!';
+  
 }
