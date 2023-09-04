7b9ca5ce917545fab4b2370595e130072866d6a0
filomenoPonce580 <filomenoponce580@gmail.com>
2023-09-04 12:03:23 -0700

    optimized array.diff for readability

--- 6kyu/array.diff/main.js
+++ 6kyu/array.diff/main.js
@@ -1,7 +1,3 @@
 function arrayDiff(a, b) {
-    if(b.length === 0) return a
-    
-    return a.filter(elem => {
-      return !b.includes(elem)
-    })
+    return a.filter(elem => !b.includes(elem))
 }
