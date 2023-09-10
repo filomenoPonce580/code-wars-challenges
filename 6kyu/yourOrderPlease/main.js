20cf8bb1c4fead0d0b0dd801cea5b4430821f905
filomenoPonce580 <filomenoponce580@gmail.com>
2023-09-10 09:27:45 -0700

    refactored

--- 6kyu/yourOrderPlease/main.js
+++ 6kyu/yourOrderPlease/main.js
@@ -1,13 +1,5 @@
 function order(words){
-    let nums= /[1-9]/g
-    let resultObj = {}
-    let result = []
-    words.split(' ').forEach((word, i)=>{
-      resultObj[Number(word.match(nums))] = word
-    })
-  
-    for(const words in resultObj){
-      result.push(resultObj[words])
-    }
-    return result.join(' ')
-}
+    return words.split(' ').sort((a, b)=>{
+        return a.match(/\d/) - b.match(/\d/);
+     }).join(' ');
+  }    
