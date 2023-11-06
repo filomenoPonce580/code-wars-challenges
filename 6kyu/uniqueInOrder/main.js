7af9aac979a96d263b676062307f091498264f9a
filomenoPonce580 <filomenoponce580@gmail.com>
2023-11-06 13:04:29 -0800

    refactored uniqueInOrder(6kyu), now using the spread operator and filter array method

--- 6kyu/uniqueInOrder/main.js
+++ 6kyu/uniqueInOrder/main.js
@@ -1,10 +1,3 @@
 function uniqueInOrder(iterable){
-    let result = [];
-    if(typeof iterable === 'string'){
-      iterable = iterable.split('')
-    }
-    iterable.forEach((char, i)=>{
-      if(char !== iterable[i+1]) result.push(char);   
-    })
-    return result
+    return [...iterable].filter((a, i) => a !== iterable[i-1])
 }
