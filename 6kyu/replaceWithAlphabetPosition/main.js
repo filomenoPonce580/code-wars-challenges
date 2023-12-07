95e3a9443fe4d5dd4290691a46301915b115129b
filomenoPonce580 <filomenoponce580@gmail.com>
2023-12-07 09:29:38 -0800

    refactored replaceWithAplhabetPosition. Function is more legible now

--- 6kyu/replaceWithAlphabetPosition/main.js
+++ 6kyu/replaceWithAlphabetPosition/main.js
@@ -1,6 +1,9 @@
 function alphabetPosition(text) {
     let alph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    return text.toLowerCase().replace(/[^A-Za-z]/g, '').split('').map((char, i)=>{
-      return alph.indexOf(char) + 1
-    }).join(' ');
+    return text.toLowerCase()
+                .replace(/[^A-Za-z]/g, '')
+                .split('').map((char)=>{
+                    return alph.indexOf(char) + 1
+                })
+                .join(' ');
 }
